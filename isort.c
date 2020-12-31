ba732180ad2710658380bce9168a0e8359692b03
omerMichael <74428768+omerMichael@users.noreply.github.com>
2020-12-31 10:09:11 +0200

    Update isort.c

--- isort.c
+++ isort.c
@@ -1,183 +1,74 @@
+#define size_arr 50
 #include <stdio.h>
-#include <string.h>
-#define LINE 256
-#define WORD 30
-#define LINES 250
 
-
-// This function need to check if str contain word
-
-int substring (char *str1, char *str2)
-{
-  int i=0;
-
-  while (*str1!='\0')
-  {
-     if (*str1 == *(str2+i))
-     { //check if the char  is equals
-        i++;
-        if (strlen(str2)==i)
-        	return 1; //return true
-     }
-     else
-    	 i=0;
-
-     str1++;
-   }
-return 0; // return false
-
-}
-
-
-
-
-// This functin check if the word similiar until num of change to str
-int similar (char *s, char *t, int n)
-{
-	int i=0;
-    int j=-1;// num of mistake
-
-    if (strlen(s)>strlen(t)+n)
-    	return 0;
-
-    while (*s!='\0')
+void printArray(int *arr, int n) 
+{ 
+    int i; 
+    for (i = 0; i < n-1; i++)
     {
-    	if (*s == *(t+i))
-    	{
-    		i++;
-    		if (strlen(t)==i)
-    			return 1;
-    	}
+        printf("%d,", *(arr+i));
+    }// end for
 
-    	else // if the char is not equals
-    	{
-    		j++; // num of mistake
-            if (j==n)
-              {
-                 i=0;
-                 j=0;
-              }
+printf("%d", *(arr+i));
+} 
 
-    	 }
+//***************************************************
+void shift_element(int *arr, int i)
+{
+for (int j=i-1; j>0; j--)
+	*( arr+1+j)=*(arr+j);
+ //end for
+}
+//**************************************************
+void insertion_sort(int *arr, int len)
+{ 
+    int val,i , j; 
+    for (i = 1; i < len; i++)
+    {
+       val= *(arr+i);
+        j = i - 1; 
+       // int counter=0;
+        //here we need to chenge with shift element
+        while ( (*(arr+j) > val) && j>=0)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+         
+         
+            // until here
+         //end while
+       *(arr +j+1) = val; 
+    } //end for
+ }//end functionn
 
-       s++; // pointers ++
-     }
+//**********************************************
+  int main() 
+  {
+    int arr[size_arr];
+    int a;
+
+    //input
+    // printf ("enter 50 number into array: \n");
+
+    for(int i=0 ; i<size_arr; i++)
+    {
+    	scanf("%d", &a);
+    	*(arr+i)=a;
+  	}
+
+    //output
+    // printf("Before Arr \n");
+    // printArray(arr, size_arr); 
+    
+    insertion_sort(arr, size_arr);
+
+    // printf("After insetionSort \n");
+    printArray(arr, size_arr);
+
+     //shift_element(arr, size_arr);
+     //printf("After shift_element \n");
+     //printArray(arr, size_arr);
 
 return 0;
-}
-
-//*************************************
-
-int getLine(char s[])
-{
-	int SizeLine = 0;
-	char letter = getchar();
-
-		while(letter=='\n')  //remove the spare whitelines in the begening of the file.
-			letter = getchar();
-
-	if(letter==EOF) //if the input is empty.
-		return 0;
-
-	while((letter != EOF) &&(letter != '\n') && (SizeLine < LINE))
-	{
-		s[SizeLine]=letter;
-		SizeLine++;
-		letter = getchar();
-	}
-	s[SizeLine]='\0'; // end of the srtring;
-
-	return SizeLine;
-
-}
-
-//*********************************************************
-
-int getword(char w[])
-{
-	int SizeWord = 0;
-	char letter = getchar();
-
-	while((letter != EOF)&&(letter != ' ') &&(letter != '\t')&&(letter != '\n') && (SizeWord < WORD))
-	{
-		w[SizeWord]=letter;
-		SizeWord++;
-		letter = getchar();
-	}
-
-
-
-	w[SizeWord]='\0'; // end of the srtring;
-
-
-	return SizeWord;
-
-}
-
-//*****************************************************
-
-
-void print_lines(char * str)
-{
-	char line[LINE];
-	int SizeLine = getLine(line);
-
-	while(SizeLine != 0)
-	{
-		if(substring(line, str)==1)
-			printf("%s\n", line);
-
-		SizeLine = getLine(line); //line.next();
-	}
-
-}
-
-//*****************************************************
-
-void print_similar_words(char * str)
-{
-	char word[WORD];
-	int SizeWord = getword(word);
-
-
-	while(SizeWord != 0)
-	{
-
-		if(similar(word, str,1)==1)
-			printf("%s\n", word); //check it
-
-
-		SizeWord = getword(word); //word.next();
-	}
-
-
-}
-
-//******************************************************
-
-int main ()
-{
-
-   //part A
-   char word[WORD];
-   char option;
-
-   getword(word); //take the first word in the first line
-
-
-
-   scanf(" %c\n" , &option);
-
-
-
-
-   //Part B
-
-   	if(option == 'a')
-   		print_lines(word);
-
-   	else if(option == 'b')
-   		print_similar_words(word);
-
-
-}
+  }
