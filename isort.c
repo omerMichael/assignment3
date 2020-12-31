353503b7fdee676ddae8590b6c9fd3dd00431a6d
omerMichael <74428768+omerMichael@users.noreply.github.com>
2020-12-31 21:44:16 +0200

    final

--- isort.c
+++ isort.c
@@ -1,74 +1,55 @@
-#define size_arr 50
 #include <stdio.h>
+#include <stdlib.h>
 
-void printArray(int *arr, int n) 
-{ 
-    int i; 
-    for (i = 0; i < n-1; i++)
-    {
-        printf("%d,", *(arr+i));
-    }// end for
+#define LEN 50
 
-printf("%d", *(arr+i));
-} 
+int shift_element(int *arr, int i) {
+    for (int j = i; j >= 0; j--) {
+        *(arr + j + 1) = *(arr + j);
 
-//***************************************************
-void shift_element(int *arr, int i)
-{
-for (int j=i-1; j>0; j--)
-	*( arr+1+j)=*(arr+j);
- //end for
+    }
+    return 0;
 }
-//**************************************************
-void insertion_sort(int *arr, int len)
-{ 
-    int val,i , j; 
-    for (i = 1; i < len; i++)
-    {
-       val= *(arr+i);
-        j = i - 1; 
-       // int counter=0;
-        //here we need to chenge with shift element
-        while ( (*(arr+j) > val) && j>=0)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+
+int insertion_sort(int *arr, int len) {
+    for (int i = 1; i < len; ++i) {
+        int t = *(arr + i);
+        int count = 0;
+        int j = i - 1;
+        while (j >= 0 && *(arr + j) > t) {
+            count++;
+            j--;
         }
-         
-         
-            // until here
-         //end while
-       *(arr +j+1) = val; 
-    } //end for
- }//end functionn
+        shift_element((arr + i - count), count - 1);
+        *(arr + i - count) = t;
+    }
+    return 0;
+}
 
-//**********************************************
-  int main() 
-  {
-    int arr[size_arr];
-    int a;
+int printarr(int *a) {
+    for (size_t i = 0; i < LEN; i++) {
+        if (i == 0) {
+            printf("%d", *(a));
+        } else {
+            printf(",%d", *(a + i));
+        }
 
-    //input
-    // printf ("enter 50 number into array: \n");
+    }
+    printf("\n");
+    return 0;
+}
 
-    for(int i=0 ; i<size_arr; i++)
-    {
-    	scanf("%d", &a);
-    	*(arr+i)=a;
-  	}
+int main() {
+   
+    int arr[LEN];
+    printf("insert 50 numbers\n");
+    for (int i = 0; i < LEN; i++) {
+        scanf("%d", (arr + i));
 
-    //output
-    // printf("Before Arr \n");
-    // printArray(arr, size_arr); 
-    
-    insertion_sort(arr, size_arr);
 
-    // printf("After insetionSort \n");
-    printArray(arr, size_arr);
-
-     //shift_element(arr, size_arr);
-     //printf("After shift_element \n");
-     //printArray(arr, size_arr);
-
-return 0;
-  }
+    }
+    printarr(arr);
+    insertion_sort(arr,LEN);
+    printarr(arr);
+    return 0;
+}
