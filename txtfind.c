60d752d2fc0aee1d1a466356b7df4b79aacfb4de
omerMichael <74428768+omerMichael@users.noreply.github.com>
2020-12-31 09:51:03 +0200

    Update txtfind.c

--- txtfind.c
+++ txtfind.c
@@ -1,121 +1,169 @@
+//
+// Created by omerm on 31/12/2020.
+//
+
+
 #include <stdio.h>
 #include <string.h>
 #define LINE 256
 #define WORD 30
 #define LINES 250
 
-// if the word contain str1 the words from the user, and str2 if contaion str1
-//it's work now
-int substring( char * str1, char * str2){
-    //This function returns a pointer to the first occurrence in haystack of any
-    // of the entire sequence of characters specified in needle, or a null pointer if the sequence is not present in haystack.
-    if(strstr(str1, str2) != NULL) {
-        return 1;
-    }
-    return 0;
 
-}
+// This function need to check if str contain word
 
-int similar (char *s, char *t, int n){
-    if (substring(s,t))
-        return 1;
+int substring (char *str1, char *str2)
+{
     int i=0;
-    while (*(s+i) != EOF && *(t+i) != EOF && *(s+i)!= ' ' && *(t+i) != ' ' ){
-       //check if the first char in s equal to first char in t
-       int p =n;
-       int m=0;
-        if (*(s+i)==*(t)) {
-            for (int j = 0; *(s + j+i) != ' ' && *(t + j)-m != ' '; j++) {
-            
-            if (*(s+j+i)!=*(t+j-m)){
-            p--;
-            m++;
-            }
-            if (p==0)
-                break;
-            if (*(t+j+1)==' ' ||*(t+j+1)==EOF)
-                return 1;
-            }
-        }// end if
-        i++;
+
+    while (*str1!='\0')
+    {
+        if (*str1 == *(str2+i))
+        { //check if the char  is equals
+            i++;
+            if (strlen(str2)==i)
+                return 1; //return true
+        }
+        else
+            i=0;
+
+        str1++;
     }
-    return 0;
+    return 0; // return false
 
 }
+
+
+
+
+// This functin check if the word similiar until num of change to str
+int similar (char *s, char *t, int n)
+{
+    // check the size of wors, if it's bigger return false
+    if (strlen(s)>strlen(t)+n)
+        return 0;
+     int j=-1;// num of mistake
+    int i=0;
+   
+
+    int m =0;
+    while (*(s+m)!='\0')
+    {
+        //check cahe equal
+        if (*s == *(t+i))
+        {
+            i++;
+            // if i is equal to size of word return true
+            if (i==strlen(t))
+                return 1;
+        }
+
+        else // if the char is not equals
+        {
+            j++; // num of mistake increase 1
+            if (j==n)
+            {
+                j=0;
+                i=0;
+              
+            }
+
+        }
+        // m++;
+        s++; // pointers ++
+    }
+
+    return 0;
+}
+
+//*************************************
+
 int getLine(char s[])
 {
-	
-	char letter = getchar();
     int SizeLine = 0;
-		// while(letter=='\n')  //remove the spare whitelines in the begening of the file.
-		// 	letter = getchar();
+    char letter = getchar();
 
-	if(letter==EOF) //if the input is empty.
-		return 0;
+    while(letter=='\n')  //remove the spare whitelines in the begening of the file.
+        letter = getchar();
 
-	while((letter != EOF) &&(letter != '\n') && (SizeLine < LINE))
-	{
-		s[SizeLine]=letter;
-		SizeLine++;
-		letter = getchar();
-	}
-	s[SizeLine]='\0'; // end of the srtring;
+    if(letter==EOF) //if the input is empty.
+        return 0;
 
-	return SizeLine;
+    while((letter != EOF) &&(letter != '\n') && (SizeLine < LINE))
+    {
+        s[SizeLine]=letter;
+        SizeLine++;
+        letter = getchar();
+    }
+    s[SizeLine]='\0'; // end of the srtring;
+
+    return SizeLine;
 
 }
+
+//*********************************************************
+//function to put a a word in w
+int getWord(char w[])
+{
+  int SizeWord = 0;
+	char letter = getchar();
+
+	while((letter != EOF)&&(letter != ' ') &&(letter != '\t')&&(letter != '\n') && (SizeWord < WORD))
+	{
+		w[SizeWord]=letter;
+		SizeWord++;
+		letter = getchar();
+	}
+
+
+
+	w[SizeWord]='\0'; // end of the srtring;
+
+
+	return SizeWord;
+
+}
+
+//*****************************************************
+
 
 void print_lines(char * str)
 {
     char line[LINE];
-	// int SizeLine = getLine(line); // using function for getLine, now the line in char line
-     int SizeLine =1;
-	while(SizeLine > 0)
-	{
-        //get a line
-        SizeLine = getLine(line); //line.next();
-        //call to function that check if it's
-		if(substring(line, str)==1)
-        // print the line
-			printf("%s\n", line);
-        
-		
-	}
-}
-int getword(char w[])
-{
-    int SizeWord = 0;
-    char letter = getchar();
+    int SizeLine = getLine(line);
 
-    while((letter != EOF)&&(letter != ' ') &&(letter != '\t')&&(letter != '\n') && (SizeWord < WORD))
+    while(SizeLine != 0)
     {
-        w[SizeWord]=letter;
-        SizeWord++;
-        letter = getchar();
+        if(substring(line, str)==1)
+            printf("%s\n", line);
+
+        SizeLine = getLine(line); //line.next();
     }
 
-
-
-    w[SizeWord]='\0'; // end of the srtring;
-
-
-    return SizeWord;
-
 }
+
+//*****************************************************
+
 void print_similar_words(char * str)
 {
     char word[WORD];
-    int sizeWord = 1;
-    while (sizeWord>0){
-        sizeWord = getword(word);
-        if(similar(word,str,1)==1)
-        printf("%s\n", word);
+//    int SizeWord = getWord(word);
 
+
+    while(getWord(word) != 0)
+    {
+
+        if(similar(word, str,1)==1)
+            printf("%s\n", word); //check it
+
+
+//        SizeWord = getWord(word); //word.next();
     }
-    
+
+
 }
 
-
+//******************************************************
 
 int main ()
 {
@@ -124,7 +172,7 @@ int main ()
     char word[WORD];
     char option;
 
-    getword(word); //take the first word in the first line
+    getWord(word); //take the first word in the first line
 
 
 
@@ -143,4 +191,3 @@ int main ()
 
 
 }
-
